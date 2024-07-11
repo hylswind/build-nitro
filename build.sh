a5737e7f149080156fd139070d19198dc0f38a71
Tim Liu <7122621+hylswind@users.noreply.github.com>
2024-07-11 09:53:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ git clone https://github.com/karthequian/docker-helloworld.git
 
 cd docker-helloworld
 docker build --tag hello:latest .
-nitro-cli build-enclave --docker-uri hello:latest --output-file /artifact/hello.eif
+nitro-cli build-enclave --docker-uri hello:latest --output-file /artifact/hello.eif > /artifact/result.txt
